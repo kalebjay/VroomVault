ac682ef0a82f47ee6327403a94976401e967f9c5
kaleb <kalebjay@gmail.com>
2026-01-14 13:50:12 -0500

    adding .split in origins to accept various urls

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 origins = ['http://localhost:5173', 'http://127.0.0.1:5173']
 
 if os.getenv("ALLOWED_ORIGINS"):
-    origins.append(os.getenv("ALLOWED_ORIGINS"))
+    origins.extend(os.getenv("ALLOWED_ORIGINS").split(","))
 
 app.add_middleware(
     CORSMiddleware,
